f2c8dbc464823e607aa19e0e50c66766cf7b5f30
Erik Hollensbe <git@hollensbe.org>
2024-04-25 16:00:29 -0700

    moo
    Signed-off-by: Erik Hollensbe <git@hollensbe.org>

--- src/lib.rs
+++ src/lib.rs
@@ -22,19 +22,46 @@ pub struct Data {
     volatility: f32,
 }
 
+fn pick_speed() -> usize {
+    rand::random::<usize>() % MAX_SPEED
+}
+
+fn pick_color() -> Color {
+    COLORS[rand::random::<usize>() % COLORS.len()]
+}
+
+fn pick_volatility() -> f32 {
+    rand::random::<f32>() % 1.0
+}
+
+fn pick_symbol(corpus: &Corpus) -> char {
+    corpus[rand::random::<usize>() % corpus.len()]
+}
+
+fn get_corpus() -> Result<Corpus> {
+    let mut v = Vec::new();
+    std::io::stdin().read_to_end(&mut v)?;
+
+    Ok(v.iter().map(|x| (*x).into()).collect::<Corpus>())
+}
+
 impl Data {
-    pub fn new(symbol: char, window: Window) -> Self {
+    pub fn new(window: &Window) -> Self {
         Self {
             position: Position(rand::random::<usize>() % window.width, window.height),
-            symbol,
-            speed: rand::random::<usize>() % MAX_SPEED,
-            color: next_color(),
-            volatility: rand::random::<f32>() % 1.0,
+            symbol: pick_symbol(&window.corpus),
+            speed: pick_speed(),
+            color: pick_color(),
+            volatility: pick_volatility(),
         }
     }
 
     pub fn iterate(&mut self) {
-        if (rand::random::<f32>() % 1.0) > self.volatility {}
+        if (rand::random::<f32>() % 1.0) > self.volatility {
+            self.speed = pick_speed();
+            self.color = pick_color();
+            self.volatility = pick_volatility();
+        }
     }
 }
 
@@ -45,28 +72,30 @@ pub struct Position(usize, usize);
 
 #[derive(Debug, Clone)]
 pub struct Window {
+    corpus: Corpus,
     buffer: Vec<Data>,
     height: usize,
     width: usize,
 }
 
 impl Window {
-    pub fn new(height: usize, width: usize) -> Self {
-        Self {
+    pub fn new(height: usize, width: usize) -> Result<Self> {
+        Ok(Self {
+            corpus: get_corpus()?,
             height,
             width,
             buffer: Vec::with_capacity(height * width),
-        }
+        })
     }
 
     pub fn from_terminal() -> Result<Self> {
         let ws = crossterm::terminal::window_size()?;
-        Ok(Self::new(ws.height.into(), ws.width.into()))
+        Self::new(ws.height.into(), ws.width.into())
     }
 
-    pub fn draw_next(&self, corpus: Corpus) -> Result<()> {
-        let data = self.next_data(corpus);
-        let color = next_color();
+    pub fn draw_next(&self) -> Result<()> {
+        let data = self.next_data();
+        let color = pick_color();
 
         std::io::stdout()
             .execute(MoveTo(
@@ -78,18 +107,7 @@ impl Window {
         Ok(())
     }
 
-    fn next_data(&self, corpus: Corpus) -> Data {
-        Data::new(corpus[rand::random::<usize>() % corpus.len()], self.clone())
+    fn next_data(&self) -> Data {
+        Data::new(self)
     }
-}
-
-fn next_color() -> Color {
-    COLORS[rand::random::<usize>() % COLORS.len()]
-}
-
-fn get_corpus() -> Result<Corpus> {
-    let mut v = Vec::new();
-    std::io::stdin().read_to_end(&mut v)?;
-
-    Ok(v.iter().map(|x| (*x).into()).collect::<Corpus>())
 }
