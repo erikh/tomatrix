1d425131664ca336b8d41a2ee5eb6b764a172025
Erik Hollensbe <git@hollensbe.org>
2024-04-24 22:26:49 -0700

    still sucks
    Signed-off-by: Erik Hollensbe <git@hollensbe.org>

--- src/lib.rs
+++ src/lib.rs
@@ -7,21 +7,32 @@ use crossterm::{
 };
 use std::io::Read;
 
+pub const MAX_SPEED: usize = 4;
+
 lazy_static::lazy_static! {
     pub static ref COLORS: [Color; 4] = [DarkGreen, DarkGrey, Green, White];
 }
 
-// stdout()
-//     .execute(MoveTo(x as u16, y as u16))?
-//     .execute(SetForegroundColor(self.grid[y][x].color()))?
-//     .execute(Print(&format!("{}", self.grid[y][x].character())))?;
-//
-pub type Corpus = Vec<char>;
+pub struct Data {
+    position: Position,
+    symbol: char,
+    speed: usize,
+    color: Color,
+    volatility: f32,
+}
+
+impl Data {
+    fn iterate(&mut self) {
+        if (rand::random::<f32>() % 1.0) > self.volatility {}
+    }
+}
+
+pub type Corpus = Vec<Data>;
 
 pub struct Position(usize, usize);
 
 pub struct Window {
-    buffer: Vec<char>,
+    buffer: Vec<Data>,
     height: usize,
     width: usize,
 }
@@ -41,26 +52,21 @@ impl Window {
     }
 
     pub fn draw_next(&self, corpus: Corpus) -> Result<()> {
-        let ch = next_char(corpus);
-        let pos = self.next_position();
+        let data = next_data(corpus);
         let color = next_color();
 
         std::io::stdout()
-            .execute(MoveTo(pos.0.try_into()?, pos.1.try_into()?))?
+            .execute(MoveTo(
+                data.position.0.try_into()?,
+                data.position.1.try_into()?,
+            ))?
             .execute(SetForegroundColor(color))?
-            .execute(Print(&format!("{}", ch)))?;
+            .execute(Print(&format!("{}", data.symbol)))?;
         Ok(())
-    }
-
-    fn next_position(&self) -> Position {
-        Position(
-            rand::random::<usize>() % self.width,
-            rand::random::<usize>() % self.height,
-        )
     }
 }
 
-fn next_char(corpus: Corpus) -> char {
+fn next_data(corpus: Corpus) -> Data {
     corpus[rand::random::<usize>() % corpus.len()]
 }
 
